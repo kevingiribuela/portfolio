463e0aa00ad7a7ee52f5e898050dabdc668bb223
Kevin Giribuela <kevin.giribuela@falcoprocessing.com>
2023-09-28 13:16:07 -0300

    Se corrije bug de desplazamiento logico y se agregan assertions

--- Verilog/TP1/fir/fir.v
+++ Verilog/TP1/fir/fir.v
@@ -1,3 +1,5 @@
+`timescale 1ns/1ps 
+
 module fir #(
     parameter NB_DATA = 8
 )
@@ -9,18 +11,18 @@ module fir #(
     output [NB_DATA-1:0]    o_data
 );
 
-wire [NB_DATA-1:0]  x0, y0;
+wire [NB_DATA-1:0] y0;
 
-reg [NB_DATA-1:0]   x1, x2, x3, y1, y2;
+reg [NB_DATA-1:0]   x0, x1, x2, x3, y1, y2;
 
-assign x0 = i_data;
-assign y0 = x0 - x1 + x2 + x3 + y1>>1 + y2>>2;
+assign y0 = x0 - x1 + x2 + x3 + (y1>>1) + (y2>>2);
 
 always @(posedge i_clk) begin
     if(i_rst) begin
         y1 <= 0;
         y2 <= 0;
-
+    
+        x0 <= 0;
         x1 <= 0;
         x2 <= 0;
         x3 <= 0;
@@ -29,9 +31,11 @@ always @(posedge i_clk) begin
         y1 <= y0;
         y2 <= y1;
 
+        x0 <= i_data;
         x1 <= x0;
         x2 <= x1;
         x3 <= x2;
+        
     end
 end
 
--- Verilog/TP1/fir/fir_tb.v
+++ Verilog/TP1/fir/fir_tb.v
@@ -1,4 +1,10 @@
 `timescale 1ns/1ps 
+`define T_clock 4
+`define assert(signal, value) \
+        if (signal !== value) begin \
+            $display("ASSERTION FAILED in %m: signal != value"); \
+            $finish; \
+        end
 
 module fir_tb #(
     parameter NB_DATA = 8
@@ -22,17 +28,43 @@ fir
 
 initial begin
     i_data = 1;
-    i_clk   = 0;
-    i_rst = 1;
+    i_clk  = 0;
+    i_rst  = 1;
     # 10;
-    i_rst = 0;
-    # 20;
-    i_data = 3;
-    # 2;
+    i_rst  = 0;
+    # `T_clock;
+    i_data = 2;
+    # `T_clock;
+    `assert(o_data, 8'd1);
     i_data = 1;
-    # 2;
+    # `T_clock;
+    `assert(o_data, 8'd0);
+    i_data = 2;
+    # `T_clock;
+    `assert(o_data, 8'd4);
+    i_data = 1;
+    # `T_clock;
+    `assert(o_data, 8'd4);
+    i_data = 2;
+    # `T_clock;
+    `assert(o_data, 8'd7);
     i_data = 0;
-    # 300;
+    # `T_clock;
+    `assert(o_data, 8'd5);
+    # `T_clock;
+    `assert(o_data, 8'd6);
+    # `T_clock;
+    `assert(o_data, 8'd6);
+    # `T_clock;
+    `assert(o_data, 8'd4);
+    # `T_clock
+    `assert(o_data, 8'd3);
+    # `T_clock;
+    `assert(o_data, 8'd2);
+    # `T_clock;
+    `assert(o_data, 8'd1);
+    # `T_clock;
+    `assert(o_data, 8'd0);
     $finish;
 end
 
